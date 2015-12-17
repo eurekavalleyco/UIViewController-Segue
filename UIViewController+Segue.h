2928d13c7a04a3a668d0fef4db3ef855f8980361
Eureka Valley Co <eurekavalleyco@gmail.com>
2015-12-17 13:28:05 -0500

    Adds files.

---
+++ UIViewController+Segue.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Segue.h
+//  UIViewController+Segue
+//
+//  Created by Ken M. Haggerty on 12/17/15.
+//  Copyright © 2015 MCMDI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Segue)
+
+@end
---
+++ UIViewController+Segue.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Segue.m
+//  UIViewController+Segue
+//
+//  Created by Ken M. Haggerty on 12/17/15.
+//  Copyright © 2015 MCMDI. All rights reserved.
+//
+
+#import "UIViewController+Segue.h"
+
+@implementation UIViewController (Segue)
+
+@end
